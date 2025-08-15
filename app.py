035c5d743e8c1760932392dbacce084c06807b2b
Shreyas Shiva <shreyasshiva29@gmail.com>
2025-08-15 16:11:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import warnings
 warnings.filterwarnings('ignore')
